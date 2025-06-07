126397dc3854003f850164580715c3d829c03bf5
Athindran <rameshkumarathindran@gmail.com>
2025-06-07 10:17:05 -0400

    update target backward pass

--- simulators/policy/ilqr_reachavoid_policy.py
+++ simulators/policy/ilqr_reachavoid_policy.py
@@ -434,7 +434,8 @@ class iLQRReachAvoid(iLQR):
             # Q_xx = c_xx[:, :, idx] + fx[:, :, idx].T @ V_xx @ fx[:, :, idx]
             Q_ux_reg = fu[:, :, idx].T @ (V_xx + reg_mat) @ fx[:, :, idx]
             Q_u = c_u_t[:, idx] + fu[:, :, idx].T @ V_x
-            Q_uu_reg = fu[:, :, idx].T @ (V_xx + reg_mat) @ fu[:, :, idx]
+            Q_uu_reg = (c_uu_t[:, :, idx] + \
+                fu[:, :, idx].T @ (V_xx + reg_mat) @ fu[:, :, idx])
 
             Q_uu_inv = jnp.linalg.inv(Q_uu_reg)
             Ks = Ks.at[:, :, idx].set(-Q_uu_inv @ Q_ux_reg)            
@@ -595,7 +596,8 @@ class iLQRReachAvoid(iLQR):
             Q_ux_reg = (fu[:, :, idx].T @ (V_xx + reg_mat) @ fx[:, :, idx] + Q_ux_append)       
             Q_u = c_u_t[:, idx] + fu[:, :, idx].T @ V_x
             Q_uu_append = jnp.einsum('i, ijk->jk', V_x, fuu[:, :, :, idx])
-            Q_uu_reg = (fu[:, :, idx].T @ (V_xx + reg_mat) @ fu[:, :, idx] + Q_uu_append)
+            Q_uu_reg = (c_uu_t[:, :, idx] + \
+                fu[:, :, idx].T @ (V_xx + reg_mat) @ fu[:, :, idx])
 
             Q_uu_inv = jnp.linalg.inv(Q_uu_reg)
             Ks = Ks.at[:, :, idx].set(-Q_uu_inv @ Q_ux_reg)
