a80affd5b25b52474689f355960cf0fd0284678a
Athindran <rameshkumarathindran@gmail.com>
2025-10-31 22:43:27 -0400

    rename bicycle5d_margin

--- simulators/car/car_single.py
+++ simulators/car/car_single.py
@@ -8,7 +8,7 @@ from gym import spaces
 
 from jax import numpy as jnp
 
-from .bicycle5d_margin import BicycleReachAvoidMargin
+from .bicycle_margin import BicycleReachAvoidMargin
 from simulators.base_single_env import BaseSingleEnv
 
 
